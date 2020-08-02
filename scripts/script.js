1ff548b55f208ac781a7dd5bb01023009e555e34
Nikita Veloped <velopidr@yandex.ru>
2020-08-02 18:54:54 -0500

    nameinsertion

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,9 @@ let editButton = document.querySelector('.profile__edit');
 let popupElement = document.querySelector('.popup');
 let overlay = document.querySelector('.overlay');
 let closeButton = document.querySelector('.popup__close-button');
-
+let saveButton = document.querySelector('.form__save-button');
+let profileName = document.querySelector('.profile__name');
+let profileOccupation = document.querySelector('.profile__subtitle');
 let popupOpened = false;
 
 function popup() {
@@ -17,7 +19,18 @@ function popup() {
   }
 }
 
+function changeName() {
+  let name = document.querySelector('.form__input_name');
+  let occupation = document.querySelector('.form__input_occupation');
+
+  profileName.textContent = name.value;
+  profileOccupation.textContent = occupation.value;
+
+  popup();
+}
+
 editButton.addEventListener('click', popup);
 closeButton.addEventListener('click', popup);
+saveButton.addEventListener('click', changeName);
 
 
