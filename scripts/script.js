a9a4dd9cbe54bf3e5616d02dce1f94fb57d6fc8e
Nikita Veloped <velopidr@yandex.ru>
2020-08-14 16:14:53 -0500

    dynamic-cards

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,12 @@
-let editButton = document.querySelector('.profile__edit');
-let popupElement = document.querySelector('.popup');
-let closeButton = document.querySelector('.form__close-button');
-let form = document.querySelector('.form');
-let profileName = document.querySelector('.profile__name');
-let profileAbout = document.querySelector('.profile__about');
-let name = document.querySelector('.form__input_type_name');
-let about = document.querySelector('.form__input_type_about');
+const editButton = document.querySelector('.profile__edit');
+const popupElement = document.querySelector('.popup');
+const closeButton = document.querySelector('.form__close-button');
+const form = document.querySelector('.form');
+const profileName = document.querySelector('.profile__name');
+const profileAbout = document.querySelector('.profile__about');
+const name = document.querySelector('.form__input_type_name');
+const about = document.querySelector('.form__input_type_about');
+const gridList = document.querySelector('.grid__list');
 
 
 function togglePopup() {
@@ -30,3 +31,60 @@ function changeName() {
 editButton.addEventListener('click', togglePopup);
 closeButton.addEventListener('click', togglePopup);
 form.addEventListener('submit', changeName);
+
+function addCard(cardName, cardLink) {
+
+  const gridCard = document.createElement('li');
+  gridCard.classList.add('grid__card');
+
+  const gridImage = document.createElement('img');
+  gridImage.classList.add('grid__image');
+  gridImage.setAttribute('src', cardLink);
+  gridImage.setAttribute('alt', cardName);
+
+  const gridText = document.createElement('div');
+  gridText.classList.add('grid__text');
+
+  const gridName = document.createElement('h2');
+  gridName.classList.add('grid__name');
+  gridName.textContent = cardName;
+
+  const gridLike = document.createElement('button');
+  gridLike.classList.add('grid__like');
+
+  gridCard.append(gridImage, gridText);
+  gridText.append(gridName, gridLike);
+  gridList.append(gridCard);
+}
+
+
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
+  },
+  {
+    name: "Lake Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
+  },
+  {
+    name: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg"
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg"
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg"
+  }
+];
+
+initialCards.forEach(function(item) {
+  addCard(item.name, item.link);
+});
