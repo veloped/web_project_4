ca36f98af6096bcc0f56326e943f2c239337370b
Veloped <veloped@dummys-MacBook-Pro.local>
2020-08-21 16:08:44 -0500

    errors-edit

--- scripts/script.js
+++ scripts/script.js
@@ -16,6 +16,62 @@ const cardTemplate = document.querySelector('.card-template').content.querySelec
 const list = document.querySelector('.grid__list');
 const popupImageElement = document.querySelector('.popup_type_image');
 const imageCloseButton = document.querySelector('.form__close-button_image');
+const form = document.querySelector(".form");
+const formInput = form.querySelector(".form__input");
+const formError = form.querySelector(`#${formInput.id}-error`);
+
+const showInputError = (formElement, inputElement, errorMessage) => {
+  inputElement.classList.add("form__input_type_error");  
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add("form__input-error_active");
+  
+};
+
+const hideInputError = (formElement, inputElement) => {
+  inputElement.classList.remove("form__input_type_error");
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  errorElement.classList.remove("form__input-error_active");
+  errorElement.textContent = "";
+  
+};
+
+const checkInputValidity = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+
+function setEventListeners(formElement) {
+  const inputList = Array.from(formElement.querySelectorAll(".form__input"));
+  inputList.forEach((inputElement) => {
+  inputElement.addEventListener("input", function () {
+    checkInputValidity(formElement, inputElement);
+  });
+});
+}
+
+function enableValidation() {
+  const formList = Array.from(document.querySelectorAll(".form"));
+  formList.forEach((formElement) => {
+  formElement.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+  });
+  setEventListeners(formElement);
+});
+}
+
+enableValidation();
+
+form.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+});
+
+formInput.addEventListener("input", function() {
+  checkInputValidity(form, formInput);
+});
 
 function togglePopup(element) {
  element.classList.toggle('popup_opened');
