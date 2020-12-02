d8b32886d7ba77a298ab3789ec59839b597c88f3
veloped <velopidr@yandex.ru>
2020-12-02 23:48:21 +0300

    Popup created

---
+++ components/Popup.js
@@ -0,0 +1,35 @@
+export default class Popup {
+    constructor(popupSelector) {
+        this._popupElement = document.querySelector(popupSelector);
+        this._handleEscClose = this._handleEscClose.bind(this);
+    }
+
+    open() {
+        this._popupElement.classList.add('popup_opened');
+        document.addEventListener("keydown", this._handleEscClose);
+    }
+
+    
+    close() {
+        console.log(this._popupElement);
+        this._popupElement.classList.remove('popup_opened');
+        document.removeEventListener("keydown", this._handleEscClose);
+
+    }
+
+    _handleEscClose(evt) {
+        if (evt.key === "Escape") {
+            this.close();
+        }
+
+    }
+
+    setEventListeners() {
+        this._popupElement.addEventListener("click", (evt) => {
+            if((evt.target === document.querySelector('.popup__close-button')) || (!evt.target.closest('.form'))) {   
+            this.close();
+            }
+        });
+    }
+}
+
